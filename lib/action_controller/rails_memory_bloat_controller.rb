41be6cc0a4032265fc2505b5bbf0dfd6a969106e
Ernesto Cojulun <erniecojulun@gmail.com>
2016-03-04 12:17:43 -0800

    created controller file to load

---
+++ lib/action_controller/rails_memory_bloat_controller.rb
@@ -0,0 +1,13 @@
+class ActionController::RailsMemoryBloatController
+  # after_filter :log_memory_usage
+  def log_memory_usage
+    rss = File.read('/proc/self/statm').split(' ')[1].to_i * 4
+    records = ActiveRecordInstanceCount::HashUtils.to_sorted_array(ActiveRecord::Base.instantiated_hash)
+    records.unshift(ActiveRecord::Base.total_objects_instantiated)
+    project, user = @project.try(:id).inspect, @user.try(:id).inspect
+    tags = ["PID: #{$$}", "Project: #{@project.try(:id).inspect}",
+            "User: #{@user.try(:id).inspect}", "Mem: #{rss}",
+            "Records: (#{records.join(' | ')})"]
+    logger.info("[Memory Usage] #{tags.join(' ')}; #{request.fullpath}")
+  end
+end
