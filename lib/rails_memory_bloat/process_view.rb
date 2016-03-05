b3440964176ca3a9e992ba3ab03ae107dd1e575c
Ernesto Cojulun <erniecojulun@gmail.com>
2016-03-05 10:28:09 -0800

    template path

--- lib/rails_memory_bloat/process_view.rb
+++ lib/rails_memory_bloat/process_view.rb
@@ -4,12 +4,16 @@ class RailsMemoryBloat::ProcessView
     @processes = processes.sort{ |x,y| y.rss <=> x.rss }
   end
 
+  def path
+    Gem.loaded_specs['rails_memory_bloat'].full_gem_path
+  end
+
   def to_html
-    ERB.new(File.read('templates/index.html.erb')).result(binding)
+    ERB.new(File.read("#{path}/templates/index.html.erb")).result(binding)
   end
 
   def render(template)
-    ERB.new(File.read("templates/#{template}"),
+    ERB.new(File.read("#{path}/templates/#{template}"),
             nil, nil, "_sub#{rand(1000000000)}").result(binding)
   end
 end
