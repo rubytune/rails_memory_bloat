081d7f7a75d76e493f4f093953829d59ee527347
Sudara <sudara@alonetone.com>
2016-03-14 19:37:42 +0100

    Stick GetProcessMem in a controller class variable

--- lib/rails_memory_bloat/logger.rb
+++ lib/rails_memory_bloat/logger.rb
@@ -8,12 +8,12 @@ module RailsMemoryBloat
     end
 
     def log_memory_usage
-      @mem ||= GetProcessMem.new
+      @@mem ||= GetProcessMem.new
       records = ActiveRecordInstanceCount::HashUtils.to_sorted_array(ActiveRecord::Base.instantiated_hash)
       records.unshift(ActiveRecord::Base.total_objects_instantiated)
       project, user = @project.try(:id).inspect, @user.try(:id).inspect
       tags = ["PID: #{$$}", "Project: #{@project.try(:id).inspect}",
-              "User: #{@user.try(:id).inspect}", "Mem: #{@mem.kb}",
+              "User: #{@user.try(:id).inspect}", "Mem: #{@@mem.kb}",
               "Records: (#{records.join(' | ')})"]
       logger.info("[Memory Usage] #{tags.join(' ')}; #{request.fullpath}")
     end
